139ee91d9a8eaaf461b3e9fda3339f5d8be46a74
tqbdev <tranquocbao3897@gmail.com>
2018-09-18 16:07:35 +0700

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Header from './components/Header/index';
-import Banner from './components/Banner';
-import CategoriesBar from './components/CategoriesBar';
-import NewArrivals from './components/NewArrivals';
-import Deals from './components/Deals';
-import ShippingInformation from './components/ShippingInformation';
-import Footer from './components/Footer';
+import Header from './components/Header/';
+import Banner from './components/Banner/';
+import CategoriesBar from './components/CategoriesBar/';
+import NewArrivals from './components/NewArrivals/';
+import Deals from './components/Deals/';
+import ShippingInformation from './components/ShippingInformation/';
+import Footer from './components/Footer/';
 
 class App extends Component {
   render() {
