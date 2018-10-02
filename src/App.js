d405ecc751c4b84c1b8e253f079bce9b41ce4759
tqbdev <tranquocbao3897@gmail.com>
2018-10-02 15:38:07 +0700

    Complete requirements of week 03 and refactor code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Header from './components/Header/';
-import Banner from './components/Banner/';
-import CategoriesBar from './components/CategoriesBar/';
-import NewArrivals from './components/NewArrivals/';
-import Deals from './components/Deals/';
-import ShippingInformation from './components/ShippingInformation/';
-import Footer from './components/Footer/';
+import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
+import CategoriesBar from './components/CategoriesBar/CategoriesBar';
+import NewArrivals from './components/NewArrivals/NewArrivals';
+import Deals from './components/Deals/Deals';
+import ShippingInformation from './components/ShippingInformation/ShippingInformation';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
