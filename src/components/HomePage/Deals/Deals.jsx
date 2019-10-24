07c7a502bd80c877754a6ac858aad5213c43a101
CBernard23 <clbernard2016@icloud.com>
2019-10-24 18:42:47 -0500

    countdown timer works correctly

--- src/components/HomePage/Deals/Deals.jsx
+++ src/components/HomePage/Deals/Deals.jsx
@@ -3,6 +3,46 @@ import React, {Component} from 'react';
 import './Deals.css';
 
 export default class Deals extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 23,
+      minutes: 45,
+      hours: 15,
+      days: 3
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.countDown(), 1000);
+  }
+
+  countDown() {
+    if(this.state.hours == 0 && this.state.seconds == 0 && this.state.minutes == 0) {
+      this.setState({
+        seconds: 59,
+        minutes: 59,
+        hours: 23,
+        days: this.state.days - 1
+      })
+    } else if(this.state.seconds == 0 && this.state.minutes == 0) {
+      this.setState({
+        seconds: 59,
+        minutes: 59,
+        hours: this.state.hours - 1
+      })
+    } else if(this.state.seconds == 0) {
+      this.setState({
+        seconds: 59,
+        minutes: this.state.minutes - 1
+      })
+    } else {
+      this.setState({
+        seconds: this.state.seconds - 1
+      })
+    }
+  }
+
   render () {
     return (
       <div className="deal_ofthe_week">
@@ -20,19 +60,19 @@ export default class Deals extends Component {
                 </div>
                 <ul className="timer">
                   <li className="d-inline-flex flex-column justify-content-center align-items-center">
-                    <div id="day" className="timer_num">03</div>
+                    <div id="day" className="timer_num">{this.state.days}</div>
                     <div className="timer_unit">Day</div>
                   </li>
                   <li className="d-inline-flex flex-column justify-content-center align-items-center">
-                    <div id="hour" className="timer_num">15</div>
+                    <div id="hour" className="timer_num">{this.state.hours}</div>
                     <div className="timer_unit">Hours</div>
                   </li>
                   <li className="d-inline-flex flex-column justify-content-center align-items-center">
-                    <div id="minute" className="timer_num">45</div>
+                    <div id="minute" className="timer_num">{this.state.minutes}</div>
                     <div className="timer_unit">Mins</div>
                   </li>
                   <li className="d-inline-flex flex-column justify-content-center align-items-center">
-                    <div id="second" className="timer_num">23</div>
+                    <div id="second" className="timer_num">{this.state.seconds}</div>
                     <div className="timer_unit">Sec</div>
                   </li>
                 </ul>
